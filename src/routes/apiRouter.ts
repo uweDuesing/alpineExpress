8d547f85b8a8103f1366f3941ced1c926da377af
uweduesing <uwe.duesing@gmail.com>
2022-08-01 14:09:30 +1200

    added post + validator

--- src/routes/apiRouter.ts
+++ src/routes/apiRouter.ts
@@ -1,24 +1,33 @@
 import express from 'express';
 import {ApiController} from "../controllers/api.controller";
+import {body, validationResult} from 'express-validator';
+
 const apiRouter = express.Router();
 
 apiRouter.get('/hello', async (_req, res) => {
     const controller = new ApiController();
-    controller.helloWorld(_req, _req).then ((response) => {
+    controller.helloWorld(_req, _req).then((response) => {
         res.setHeader('Content-Type', 'application/json');
         res.end(response);
     }, error => {
         res.render('uv/indicator_error');
     });
 });
-apiRouter.post('/hello', async (_req, res) => {
-    const controller = new ApiController();
+apiRouter.post(
+    '/hello',
+    body('user').isEmail(),
 
-    controller.helloWorld(_req, _req).then ((response) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.end(response);
-    }, error => {
-        res.render('uv/indicator_error');
+    async (_req, res) => {
+        const controller = new ApiController();
+        const errors = validationResult(_req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+        controller.helloWorld(_req, _req).then((response) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(response);
+        }, error => {
+            res.render('uv/indicator_error');
+        });
     });
-});
 export default apiRouter
